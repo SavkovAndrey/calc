60f858c0c1556eb6c24a520139ed7ae93064f354
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-12-11 02:05:52 +0300

    финал

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+double num_first; // переменная для хранения первого числа из 2-х введенных
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -27,6 +29,12 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton_mul,SIGNAL(clicked()),this,SLOT(math_operations()));
     connect(ui->pushButton_min,SIGNAL(clicked()),this,SLOT(math_operations()));
     connect(ui->pushButton_plus,SIGNAL(clicked()),this,SLOT(math_operations()));
+
+    ui->pushButton_div->setCheckable(true); // возможность устанавливать "галочку"
+                                            // что кнопка "выбрана"
+    ui->pushButton_mul->setCheckable(true);
+    ui->pushButton_min->setCheckable(true);
+    ui->pushButton_plus->setCheckable(true);
 }
 
 MainWindow::~MainWindow()
@@ -42,6 +50,14 @@ void MainWindow::digits_numbers()
 QPushButton *button =(QPushButton*) sender();
    double all_numbers; // переменная для хранения числа на дисплее
    QString new_label;  // текстовая переменная, что бы использовать в setText()
+
+   // возможность записи 0.001 т.е. чисел начинающихся нулями
+   if(ui->result_show->text().contains(".") && button->text() == "0")
+   {
+       new_label = ui->result_show->text() + button->text();
+   } else
+        {
+
    all_numbers = (ui->result_show->text() + button->text()).toDouble();
 
 
@@ -49,7 +65,7 @@ QPushButton *button =(QPushButton*) sender();
    // в скобках можно передать просто переменную, но будет 6 знаков и все...
    // а можно дать еще два параметра ( включая 12 - знаков длинны)
    new_label = QString::number(all_numbers, 'g', 12);
-
+        }
    // берем наш label->показать текст-> наш указатель->
     // он через connect получает signal нужной кнопки->
     // выводим текст этой кнопки
@@ -102,18 +118,74 @@ QPushButton *button =(QPushButton*) sender();
 
 void MainWindow::math_operations()
 {
+QPushButton *button =(QPushButton*) sender(); // отслеживаем на какую кнопку нажато
 
+num_first = ui->result_show->text().toDouble(); //сохраняем первое число в переменную
+
+ui->result_show->setText(""); // стираем табло , что бы вводить второе число
+                              // после нажатия кнопки операции
+
+button->setChecked(true);  // помечаем какая кнопка нажата,
+                           // что бы в методе равенства этим воспользоваться
 }
 
 
 void MainWindow::on_pushButton_ac_clicked()
 {
+   ui->pushButton_plus->setChecked(false);
+   ui->pushButton_min->setChecked(false);
+   ui->pushButton_mul->setChecked(false);
+   ui->pushButton_div->setChecked(false); // при нажатии на = не сработают проверки
+
+   ui->result_show->setText("0");         //  на экране ставим 0
 
 }
 
 
 void MainWindow::on_pushButton_equal_clicked()
 {
+    double labelNumber, num_second;
+    QString new_label;
 
+    num_second = ui->result_show->text().toDouble();
+
+
+
+    if(ui->pushButton_plus->isChecked())
+    {
+        labelNumber = num_first + num_second;             // проводим саму операцию
+        new_label = QString::number( labelNumber, 'g', 12); // переводим в стринг
+        ui->result_show->setText(new_label);                // выводим на Лэйбл
+        ui->pushButton_plus->setChecked(false);             // ставим галочку в фолс
+
+    }else if(ui->pushButton_min->isChecked())
+    {
+        labelNumber = num_first - num_second;
+        new_label = QString::number( labelNumber, 'g', 12);
+        ui->result_show->setText(new_label);
+        ui->pushButton_plus->setChecked(false);
+
+    }else if(ui->pushButton_div->isChecked())
+    {
+            if(num_second == 0)
+            {
+                ui->result_show->setText("0");
+            }else
+            {
+                labelNumber = num_first / num_second;
+                new_label = QString::number( labelNumber, 'g', 12);
+                 ui->result_show->setText(new_label);
+            }
+
+        ui->pushButton_plus->setChecked(false);
+
+    }else if(ui->pushButton_mul->isChecked())
+    {
+
+        labelNumber = num_first * num_second;
+        new_label = QString::number( labelNumber, 'g', 12);
+        ui->result_show->setText(new_label);
+        ui->pushButton_plus->setChecked(false);
+    }
 }
 
